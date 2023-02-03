0ad61893c948bf82e46e94962c9ac295c8251173
zhammann2 <zhammann2@huskers.unl.edu>
2023-02-03 12:33:54 -0600

    Changed GitLab to use a helper class.

--- src/GitLab.java
+++ src/GitLab.java
@@ -1,6 +1,7 @@
 
 public class GitLab {
     public static void main(String[] args) {
-        System.out.println("This is a lesson on git.");
+        GitLabHelper helper = new GitLabHelper("Zach", "Aditya");
+        helper.PrintWelcomeMessage();
     }
 }
